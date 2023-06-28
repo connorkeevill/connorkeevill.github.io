f040d4dd5f647595f5fd307421306c70fdd5773b
Connor Keevill <connor.keevill.19@gmail.com>
2023-06-28 14:10:14 +0100

    Reposition terminal and make name larger

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ import sleep from "./js/utils.js";
 
 document.addEventListener('DOMContentLoaded', async function() {
   const commandElement = document.getElementById('command');
-  const outputElement = document.getElementById('output');
+  const commandCursorElement = document.getElementById('command-cursor');
+  const outputElement = document.getElementById('name');
+  const outputCursorElement = document.getElementById('output-cursor');
 
   const command = 'whoami';
-  const name = "Connor Keevill";
+  const name = "Connor\nKeevill";
 
   await typeTextInElement(command, commandElement);
   await sleep(300);
+  commandCursorElement.style.display = "none";
   await typeTextInElement(name, outputElement);
+  outputCursorElement.style.display = "block";
 });
