d9bf7329f39e45bbe973bbacccfd6156d8378573
Connor Keevill <connor.keevill.19@gmail.com>
2023-07-08 18:44:47 +0100

    Create the tilting card effect on the terminal

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 import typeTextInElement from "./js/terminal.js";
 import sleep from "./js/utils.js";
 
+  $('.tilting-card').each(function() {
+    const card = $(this);
+  
+    $(document).on('mousemove', function(event) {
+      const cardRect = card[0].getBoundingClientRect();
+      const mouseX = event.clientX;
+      const mouseY = event.clientY;
+  
+      const cardCenterX = cardRect.left + cardRect.width / 2;
+      const cardCenterY = cardRect.top + cardRect.height / 2;
+  
+      const mouseCardX = mouseX - cardCenterX;
+      const mouseCardY = mouseY - cardCenterY;
+      
+      card.css('--mouse-x', mouseCardX / (cardRect.width / 2));
+      card.css('--mouse-y', mouseCardY / (cardRect.height / 2));
+    });
+  });
+  
 document.addEventListener('DOMContentLoaded', async function() {
   const commandElement = document.getElementById('command');
   const commandCursorElement = document.getElementById('command-cursor');
